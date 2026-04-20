8b0d508b6b3096068bf32aca8f1254adc367bce8
Kirill2245 <publickirill2245@gmail.com>
2026-04-20 16:38:15 +0500

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    prerender: false,
+  },
   images: {
     unoptimized: true,
     qualities: [75, 90],
